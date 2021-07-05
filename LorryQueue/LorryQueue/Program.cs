2e63a204ac8136d87e148c1c382ead1e9ad43e19
Kamichalik1 <41804283+Kamichalik1@users.noreply.github.com>
2021-07-05 19:56:13 +0100

    Update Program.cs

--- LorryQueue/LorryQueue/Program.cs
+++ LorryQueue/LorryQueue/Program.cs
@@ -25,10 +25,7 @@ namespace LorryQueue
         }
 
         static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-
+        {           
             Random Rand = new Random();
 
             List<LorryCustomer> Lorries = new List<LorryCustomer>();
